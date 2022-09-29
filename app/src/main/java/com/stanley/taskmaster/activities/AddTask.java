1707c09ae27ae845665fb14fcd5a52536cf9336b
Stanley <stanley.wilder.24@gmail.com>
2022-09-28 19:31:54 -0700

    lab 33

--- app/src/main/java/com/stanley/taskmaster/activities/AddTask.java
+++ app/src/main/java/com/stanley/taskmaster/activities/AddTask.java
@@ -2,20 +2,34 @@ package com.stanley.taskmaster.activities;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.amplifyframework.api.graphql.model.ModelQuery;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.generated.model.Task;
 import com.stanley.taskmaster.R;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
 public class AddTask extends AppCompatActivity {
+
+    public static final String Tag = "AddTaskActivity";
+    Spinner taskSpinner = null;
+    CompletableFuture<List<Task>> taskFuture = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
-
+        setTaskSpinner();
 
 
         Button taskSubmitButton = AddTask.this.findViewById(R.id.taskSubmitButton);
@@ -29,5 +43,22 @@ public class AddTask extends AppCompatActivity {
                 Toast toast = Toast.makeText(submit, submittedText, duration);
             }
         });
+    }
+    private void setTaskSpinner(){
+        Amplify.API.query(
+                ModelQuery.list(Task.class),
+                success -> {
+                    Log.i(Tag, "Read Tasks successfully");
+                    ArrayList<String> team = new ArrayList<>();
+                    ArrayList<Task> teams = new ArrayList<>();
+                    for(Task task : success.getData()){
+                        teams.add(task);
+                        team.add(teams.)
+                    }
+                });
+    },
+    failure -> {
+        taskFuture.complete(null);
+        Log.i(Tag, "Task not read successfully");
     }
 }
